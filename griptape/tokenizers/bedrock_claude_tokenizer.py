0a912bdfb9ba341bda0a3b41a732beff8b3e7b5d
Collin Dutter <collin@griptape.ai>
2023-09-28 16:47:51 -0700

    Use parent max tokens

--- griptape/tokenizers/bedrock_claude_tokenizer.py
+++ griptape/tokenizers/bedrock_claude_tokenizer.py
@@ -5,4 +5,3 @@ from griptape.tokenizers import AnthropicTokenizer
 @define(frozen=True)
 class BedrockClaudeTokenizer(AnthropicTokenizer):
     DEFAULT_MODEL = 'anthropic.claude-v2'
-    DEFAULT_MAX_TOKENS = 8192
