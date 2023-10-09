05e57d5beeede173e645c04abccd33df89f5a4ae
Aodhan Roche <aodhan@griptape.ai>
2023-10-09 11:40:49 -0700

    Product Description Function

--- griptape/tools/shopify_client/tool.py
+++ griptape/tools/shopify_client/tool.py
@@ -34,8 +34,8 @@ class ShopifyClient(BaseTool):
     )
     def search(self, params: dict) -> ListArtifact | ErrorArtifact:
         from requests import post, exceptions
-
-        first = params.get("product_count", 10)
+        values = params["values"]
+        first = values.get("product_count", 10)
 
         url = f"https://{self.storename}.{self.schema_endpoint}"
         body = {
@@ -64,12 +64,77 @@ class ShopifyClient(BaseTool):
                 json=body,
                 headers={"X-Shopify-Storefront-Access-Token": self.access_token},
                 timeout=self.timeout,
-            )
+            ).json()
+            if "errors" in response:
+                return ErrorArtifact(response["errors"])
+            else:
+                print(response)
+                return ListArtifact([
+                    TextArtifact(edge["node"]["title"])
+                    for edge in response["data"]["products"]["edges"]
+                ])
+        except exceptions.RequestException as err:
+            return ErrorArtifact(str(err))
 
-            products = [
-                TextArtifact(edge["node"]["title"])
-                for edge in response.json()["data"]["products"]["edges"]
-            ]
-            return ListArtifact(products)
+    @activity(
+        config={
+            "description": "Can be used to provide description of a product",
+            "schema": Schema(
+                {
+                    Literal(
+                        "product_description",
+                        description="What is the description of the product.",
+                    ): str,
+                    Literal(
+                        "first",
+                        description="Returns up to the first n elements from the list.",
+                    ): int,
+                }
+            ),
+        }
+    )
+    def description(self, params: dict) -> ListArtifact | ErrorArtifact:
+        from requests import post, exceptions
+        values = params["values"]
+        query = values.get("product_details")
+        first = values.get("first", 3)
+
+        url = f"https://{self.storename}.{self.schema_endpoint}"
+        body = {
+            "query": dedent(
+                """
+                        query searchProducts($query: String!, $first: Int) {
+                            search(query: $query, first: $first, types: PRODUCT) {
+                                edges {
+                                    node {
+                                        ...on Product {
+                                            id
+                                            title
+                                            description
+                                        }
+                                    }
+                                }
+                            }
+                        }
+            """
+            ),
+            "variables": {"query": query, "first": first}
+        }
+
+        try:
+            response = post(
+                url,
+                json=body,
+                headers={"X-Shopify-Storefront-Access-Token": self.access_token},
+                timeout=self.timeout,
+            ).json()
+            if "errors" in response:
+                return ErrorArtifact(response["errors"])
+            else:
+                print(response)
+                return ListArtifact([
+                    TextArtifact(edge["node"]["title"])
+                    for edge in response["data"]["search"]["edges"]
+                ])
         except exceptions.RequestException as err:
             return ErrorArtifact(str(err))
